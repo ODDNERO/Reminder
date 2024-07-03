49867f9b6e4fb331425861c56cbc06896aa53158
NERO <mxzxro@gmail.com>
2024-07-04 07:13:37 +0900

    ✨ [Tag] SegmentedControl에 선택된 우선순위 NotificationCenter 전달 구현

--- Reminder/Add/Priority/PriorityView.swift
+++ Reminder/Add/Priority/PriorityView.swift
@@ -9,13 +9,24 @@ import UIKit
 import SnapKit
 
 final class PriorityView: BaseView {
-
+    let prioritySegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["낮음", "중간", "높음"])
+        segmentedControl.selectedSegmentIndex = 1
+        segmentedControl.selectedSegmentTintColor = .customSky
+        return segmentedControl
+    }()
+    
     override func configureHierarchy() {
-        
+        self.addSubview(prioritySegmentedControl)
     }
     
     override func configureLayout() {
-        
+        prioritySegmentedControl.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide).inset(100)
+            $0.centerX.equalTo(self.safeAreaLayoutGuide)
+            $0.width.equalTo(200)
+            $0.height.equalTo(50)
+        }
     }
     
     override func configureView() {
--- Reminder/Add/Priority/PriorityViewController.swift
+++ Reminder/Add/Priority/PriorityViewController.swift
@@ -8,9 +8,17 @@
 import UIKit
 
 final class PriorityViewController: BaseViewController<PriorityView> {
-
+    private lazy var selectedPriorityIndex = rootView.prioritySegmentedControl.selectedSegmentIndex
+    private lazy var priority = rootView.prioritySegmentedControl.titleForSegment(at: selectedPriorityIndex)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.post(name: NSNotification.Name("toDoInfo"),
+                                        object: nil,
+                                        userInfo: ["priority": priority])
     }
 }
