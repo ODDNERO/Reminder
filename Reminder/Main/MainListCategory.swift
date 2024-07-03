9883056f208dbeb749ec61b8d305154d5db6961b
NERO <mxzxro@gmail.com>
2024-07-04 01:28:32 +0900

    ✨ MainListCategory Enum 추가

---
+++ Reminder/Main/MainListCategory.swift
@@ -0,0 +1,31 @@
+//
+//  MainListCategory.swift
+//  Reminder
+//
+//  Created by NERO on 7/3/24.
+//
+
+import UIKit
+
+enum MainListCategory: CaseIterable {
+    case Today
+    case Schedule
+    case Total
+    case Flag
+    case Done
+    
+    var attribute: (symbolImage: UIImage?, color: UIColor, title: String) {
+        switch self {
+        case .Today:
+            return (symbolImage: UIImage(systemName: "calendar"), color: .systemBlue, title: "오늘")
+        case .Schedule:
+            return (symbolImage: UIImage(systemName: "calendar"), color: .systemRed, title: "예정")
+        case .Total:
+            return (symbolImage: UIImage(systemName: "tray.fill"), color: .darkGray, title: "전체")
+        case .Flag:
+            return (symbolImage: UIImage(systemName: "flag.fill"), color: .systemYellow, title: "깃발 표시")
+        case .Done:
+            return (symbolImage: UIImage(systemName: "checkmark"), color: .systemGray, title: "완료됨")
+        }
+    }
+}
