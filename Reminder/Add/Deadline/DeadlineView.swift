40dc74962758888ddbb07cf888f3f6413eadc83d
NERO <mxzxro@gmail.com>
2024-07-04 06:16:15 +0900

    ✨ [Deadline] DatePicker에 선택된 날짜 NotificationCenter 전달 구현

--- Reminder/Add/Deadline/DeadlineView.swift
+++ Reminder/Add/Deadline/DeadlineView.swift
@@ -9,13 +9,23 @@ import UIKit
 import SnapKit
 
 final class DeadlineView: BaseView {
-
+    let deadlineDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.locale = Locale(identifier: "ko_KR")
+        datePicker.preferredDatePickerStyle = .wheels
+        return datePicker
+    }()
+    
     override func configureHierarchy() {
-        
+        self.addSubview(deadlineDatePicker)
     }
     
     override func configureLayout() {
-        
+        deadlineDatePicker.snp.makeConstraints {
+            $0.center.equalTo(self.safeAreaLayoutGuide)
+            $0.height.equalTo(self.safeAreaLayoutGuide).multipliedBy(0.5)
+        }
     }
     
     override func configureView() {
--- Reminder/Add/Deadline/DeadlineViewController.swift
+++ Reminder/Add/Deadline/DeadlineViewController.swift
@@ -8,9 +8,27 @@
 import UIKit
 
 final class DeadlineViewController: BaseViewController<DeadlineView> {
-
+    private lazy var date = rootView.deadlineDatePicker.date
+    private var deadline = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        formatDate()
+        NotificationCenter.default.post(name: NSNotification.Name("toDoInfo"),
+                                        object: nil,
+                                        userInfo: ["deadline": deadline])
+    }
+}
+
+extension DeadlineViewController {
+    private func formatDate() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy. MM. dd (EEE)"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        var stringDate = dateFormatter.string(from: date)
+        deadline = stringDate
     }
 }
