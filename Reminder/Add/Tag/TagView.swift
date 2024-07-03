9e49849cd890372633e6477236537256ea7cd09a
NERO <mxzxro@gmail.com>
2024-07-04 06:52:58 +0900

    ✨ [Tag] TextField에 작성한 태그 NotificationCenter 전달 구현

--- Reminder/Add/Tag/TagView.swift
+++ Reminder/Add/Tag/TagView.swift
@@ -9,13 +9,28 @@ import UIKit
 import SnapKit
 
 final class TagView: BaseView {
-
+    let tagTextField = {
+        let textField = UITextField()
+        textField.placeholder = " 태그를 입력해 주세요 "
+        textField.textAlignment = .center
+        textField.textColor = .black
+        textField.tintColor = .customSky
+        textField.layer.borderWidth = 2
+        textField.layer.borderColor = UIColor.customSky.cgColor
+        textField.layer.cornerRadius = 15
+        return textField
+    }()
+    
     override func configureHierarchy() {
-        
+        self.addSubview(tagTextField)
     }
     
     override func configureLayout() {
-        
+        tagTextField.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide).inset(100)
+            $0.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(30)
+            $0.height.equalTo(50)
+        }
     }
     
     override func configureView() {
--- Reminder/Add/Tag/TagViewController.swift
+++ Reminder/Add/Tag/TagViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 final class TagViewController: BaseViewController<TagView> {
-
+    private lazy var tag = rootView.tagTextField.text
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.post(name: NSNotification.Name("toDoInfo"),
+                                        object: nil,
+                                        userInfo: ["tag": tag])
+    }
 }
