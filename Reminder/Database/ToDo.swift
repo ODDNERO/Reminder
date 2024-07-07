ef044e38972c48f6f62b2194136c195a561a5a2c
NERO <mxzxro@gmail.com>
2024-07-08 04:21:31 +0900

    ✨ Todo 객체 컬럼 추가 (isComplete, isFlag)

--- Reminder/Database/ToDo.swift
+++ Reminder/Database/ToDo.swift
@@ -15,13 +15,17 @@ class ToDo: Object {
     @Persisted var deadline: String?
     @Persisted var tag: String?
     @Persisted var priority: String?
+    @Persisted var isComplete: Bool?
+    @Persisted var isFlag: Bool?
     
-    convenience init(toDoTitle: String, memo: String?, deadline: String, tag: String, priority: String) {
+    convenience init(toDoTitle: String, memo: String?, deadline: String, tag: String, priority: String, isComplete: Bool = false, isFlag: Bool = false) {
         self.init()
         self.toDoTitle = toDoTitle
         self.memo = memo
         self.deadline = deadline
         self.tag = tag
         self.priority = priority
+        self.isComplete = isComplete
+        self.isFlag = isFlag
     }
 }
