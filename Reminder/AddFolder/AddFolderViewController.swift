483073cbbbf431d597f599c6045d94828c64f09e
NERO <mxzxro@gmail.com>
2024-07-08 22:49:23 +0900

    ✨ [AddFolderVC] 추가 버튼 클릭 시 텍스트필드에 작성된 폴더 이름을 Folder Object의 Title로 저장

---
+++ Reminder/AddFolder/AddFolderViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AddFolderViewController.swift
+//  Reminder
+//
+//  Created by NERO on 7/8/24.
+//
+
+import UIKit
+
+final class AddFolderViewController: BaseViewController<AddFolderView> {
+    var delegate: AddFolderDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        settingNavigationBar()
+    }
+    
+    private func settingNavigationBar() {
+        self.title = "폴더 추가하기"
+        
+        let cancelButton = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(cancelButtonClicked))
+        cancelButton.tintColor = .systemBlue
+        navigationItem.leftBarButtonItem = cancelButton
+        
+        let addButton = UIBarButtonItem(title: "추가", style: .plain, target: self, action: #selector(addButtonClicked))
+        addButton.tintColor = .systemBlue
+        navigationItem.rightBarButtonItem = addButton
+    }
+    
+    @objc private func cancelButtonClicked() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc private func addButtonClicked() {
+        guard let title = rootView.folderNameTextField.text, !title.isEmpty else { return }
+        let folder = Folder()
+        folder.title = title
+        folder.addDate = Date()
+        dismiss(animated: true)
+    }
+}
