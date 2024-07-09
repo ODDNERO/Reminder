3669fb03fc86b6ffc59df52051b8cb54604f8c63
NERO <mxzxro@gmail.com>
2024-07-10 06:52:13 +0900

    💄 [PriorityView] 이모지 Label 추가

--- Reminder/Add/Priority/PriorityView.swift
+++ Reminder/Add/Priority/PriorityView.swift
@@ -15,9 +15,21 @@ final class PriorityView: BaseView {
         segmentedControl.selectedSegmentTintColor = .customSky
         return segmentedControl
     }()
+    let emojiLabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 70)
+        label.alpha = 0.7
+        label.textAlignment = .center
+        label.contentMode = .scaleAspectFill
+        label.layer.borderWidth = 6
+        label.layer.cornerRadius = 45
+        label.clipsToBounds = true
+        return label
+    }()
     
     override func configureHierarchy() {
         self.addSubview(prioritySegmentedControl)
+        self.addSubview(emojiLabel)
     }
     
     override func configureLayout() {
@@ -26,6 +38,11 @@ final class PriorityView: BaseView {
             $0.centerX.equalTo(self.safeAreaLayoutGuide)
             $0.width.equalTo(200)
             $0.height.equalTo(50)
+        }
+        emojiLabel.snp.makeConstraints {
+            $0.top.equalTo(prioritySegmentedControl.snp.bottom).offset(60)
+            $0.centerX.equalTo(self.safeAreaLayoutGuide)
+            $0.size.equalTo(90)
         }
     }
     
