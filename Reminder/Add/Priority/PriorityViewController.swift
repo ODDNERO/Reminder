cbd34ebbb5aabe4052a4e590631fec4ff88602fe
NERO <mxzxro@gmail.com>
2024-07-10 06:57:56 +0900

    ♻️ [PriorityVC] ViewModel 적용하여 리팩토링 / SegmentedControl 우선 순위 선택될 때마다 이모지&컬러 표시 기능 구현

--- Reminder/Add/Priority/PriorityViewController.swift
+++ Reminder/Add/Priority/PriorityViewController.swift
@@ -8,17 +8,46 @@
 import UIKit
 
 final class PriorityViewController: BaseViewController<PriorityView> {
-    private lazy var selectedPriorityIndex = rootView.prioritySegmentedControl.selectedSegmentIndex
-    private lazy var priority = rootView.prioritySegmentedControl.titleForSegment(at: selectedPriorityIndex)
+    private let viewModel = PriorityViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        bindViewModel()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        NotificationCenter.default.post(name: NSNotification.Name("toDoInfo"),
-                                        object: nil,
-                                        userInfo: ["priority": priority])
+        viewModel.inputViewWillDisappearTrigger.value = ()
+    }
+    
+    override func addEventHandler() {
+        rootView.prioritySegmentedControl.addTarget(self, action: #selector(segmentedControlChanged), for: .valueChanged)
+    }
+    @objc private func segmentedControlChanged(segmentedControl: UISegmentedControl) {
+        let selectedPriorityIndex = segmentedControl.selectedSegmentIndex
+        viewModel.inputPriority.value = Priority.allCases[selectedPriorityIndex]
+    }
+}
+
+extension PriorityViewController {
+    func bindViewModel() {
+        viewModel.outputEmoji.bind { emoji in
+            self.rootView.emojiLabel.text = emoji
+            switch emoji {
+            case "😌":
+                self.rootView.emojiLabel.layer.borderColor = UIColor.systemGreen.withAlphaComponent(0.7).cgColor
+            case "😶":
+                self.rootView.emojiLabel.layer.borderColor = UIColor.systemYellow.withAlphaComponent(0.7).cgColor
+            case "🧐":
+                self.rootView.emojiLabel.layer.borderColor = UIColor.systemRed.withAlphaComponent(0.8).cgColor
+            default:
+                self.rootView.emojiLabel.layer.borderColor = UIColor.systemGray5.cgColor
+            }
+        }
+        
+        viewModel.outputPriority.bind { priority in
+            NotificationCenter.default.post(name: NSNotification.Name("toDoInfo"),
+                                            object: nil,
+                                            userInfo: ["priority": priority])
+        }
     }
 }
